b7ec2cc196ad9133854fd357a5a0a0efe0590932
Yakub Hariana <yakubhariana70@gmail.com>
2023-09-14 21:40:35 +0700

    setting backend vercel

--- server/index.js
+++ server/index.js
@@ -1,8 +1,76 @@
-const express = require("express")
-const app= express()
-
-app.use("/", (req,res) =>{
-    res.send("Server is running.");
+require("dotenv").config({
+  path: './.env'
 });
 
-app.listen(5000, console.log("Server started on port 5000"));
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const morgan = require("morgan");
+const app = express();
+
+  // Middleware
+  app.use(cors());
+  app.use(express.json());
+  app.use(morgan("dev"));
+
+  // Get Demografi Data
+  app.get("/api/demografi-semarang", async (req, res) => {
+    try {
+      const results = await db.query(
+          `SELECT
+          json_build_object(
+              'type', 'FeatureCollection',
+              'name', 'DEMOGRAFI KOTA SEMARANG',
+              'features', json_agg(ST_AsGeoJSON(t.*)::json)
+            )
+          FROM
+          "demografi-semarang" AS t`);
+      console.log(results);
+      res.json({
+        status: "success",
+        results: results.rows.length,
+        data: {
+          demografi: results.rows,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  // Get POI Data
+  app.get("/api/poi-semarang", async (req, res) => {
+    try {
+      const results = await db.query(
+          `SELECT
+          json_build_object(
+              'type', 'FeatureCollection',
+              'name', 'POI KOTA SEMARANG',
+              'features', json_agg(ST_AsGeoJSON(t.*)::json)
+            )
+          FROM
+          "poi-semarang" AS t`);
+      console.log(results);
+      res.json({
+        status: "success",
+        results: results.rows.length,
+        data: {
+          poi: results.rows,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  app.use("/", (req,res) =>{
+    res.send("Server is running.");
+});  
+  
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`server is up and listening from port ${port}`);
+  });
+  
+  
